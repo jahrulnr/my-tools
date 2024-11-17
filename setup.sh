aed26d2e2ea5150ce0a19d86161e17de5a5fc0eb
Jahrulnr <73051140+jahrulnr@users.noreply.github.com>
2024-11-17 20:57:45 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@
 sudo docker apt update
 sudo apt-get install qemu-system binfmt-support qemu-user-static \
     docker-buildx docker-compose-v2 docker.io
+sudo sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
